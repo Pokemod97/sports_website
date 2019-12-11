b9bf1bc2793618b824a4d02521273908891bd492
Pokemod97 <cadenkline9@gmail.com>
2019-12-10 21:52:17 -0500

    format and spelling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate reqwest;
 use serde_json::Value;
 
-
-
 fn main() {
-    let resp: String =
-        reqwest::get("https://api.chess.com/pub/leaderboards").expect("Could not connect.").text().expect("Could not connect.");
+    let resp: String = reqwest::get("https://api.chess.com/pub/leaderboards")
+        .expect("Could not connect.")
+        .text()
+        .expect("Could not connect.");
     let response: Value = serde_json::from_str(&resp).unwrap();
-    let name:String = response["daily"][0]["name"].to_string().replace("\"","");
+    let name: String = response["daily"][0]["name"].to_string().replace("\"", "");
     let score = &response["daily"][0]["score"];
-    let url:String = response["daily"][0]["url"].to_string().replace("\"","");
-    println!("Today's top chess player is {} with a sccore of {}.\nCheck them out at {}", name,score,url);
-
+    let url: String = response["daily"][0]["url"].to_string().replace("\"", "");
+    println!(
+        "Today's top chess player is {} with a score of {}.\nCheck them out at {}",
+        name, score, url
+    );
 }
